2df02d79d955a461e2ac17506d73fe3857e7c708
Mauricio Lopes <mauricioleng@gmail.com>
2022-02-01 14:48:37 -0300

    App Clusterizacao V1.1

--- app/app.py
+++ app/app.py
@@ -1,16 +1,17 @@
 import pandas as pd
 import streamlit as st
 import joblib
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import power_transform
 from sklearn.cluster import KMeans
 
-st.write("Classificação de Clientes Olist")
+st.header("Classificação de Clientes Olist")
 
 st.subheader("Insira os dados para a classificação")
 
 def get_history_data():
-    data = pd.read_csv('.\\dataset\\rfm_dataset.csv')
+    data = pd.read_csv('.\\app\\dataset\\rfm_dataset.csv')
+    data.set_index('customer_unique_id')
     return data
 
 
@@ -19,38 +20,51 @@ def get_user_data():
     recency = st.sidebar.text_input("Recência")
     monetary = st.sidebar.text_input("Monetário")
     
-    user_data = {'customer_unique_id':'user'
+    user_data_dict = {'customer_unique_id':'user',
              'frequency': frequency,
              'recency': recency,
              'monetary': monetary}
 
-    features = pd.DataFrame(user_data, index='customer_unique_id')
-
+    features = pd.DataFrame(user_data_dict, index=[0])
+    features.set_index('customer_unique_id')
+    
     return features
 
-def transform_data(history_data, user_data):
-    initial_data = history_data.append(user_data)
-    data_normalized = power_transform(initial_data)
-    minmax = MinMaxScaler()
-    data_scaled = minmax.fit_transform(data_normalized)
-    return data_scaled
+def transform_data(rfm, user_data):
+    rfm.set_index('customer_unique_id', inplace = True)
+    rfm.monetary = pd.to_numeric(rfm.monetary, downcast="float")
+    Q1 = rfm.monetary.quantile(0.05)
+    Q3 = rfm.monetary.quantile(0.95)
+    IQR = Q3 - Q1
+    rfm = rfm[(rfm.monetary >= Q1 - 1.5*IQR) & (rfm.monetary <= Q3 + 1.5*IQR)]
+    initial_data = pd.concat([rfm,user_data])
+    initial_data.set_index('customer_unique_id', inplace = True)
+    #data_normalized = power_transform(initial_data)
+    #minmax = MinMaxScaler()
+    scaler = StandardScaler()
+    data_scaled = scaler.fit_transform(initial_data)
+    final_data_scaled = pd.DataFrame(data_scaled, initial_data.index, initial_data.columns)
+    return final_data_scaled
 
-def train_model(data):
-    kmeans = KMeans(n_clusters=3, init='k-means++',max_iter=300)
-    kmeans.fit(rfm )
-    return prediction
+def train_model(data_input):
+    kmeans = KMeans(n_clusters=3, init='k-means++',max_iter=300, random_state=1)
+    kmeans.fit(data_input)
+    print(kmeans.labels_)
+    data_input['cluster'] = kmeans.labels_
+    return data_input['cluster'].loc['user']
     
-user_data = get_user_data()
-history_data = get_history_data()
+user_dataset = get_user_data()
+history_dataset = get_history_data()
 if st.sidebar.button("Classificar"):
-    transformed_data = transform_data(history_data, user_data)
+    transformed_data = transform_data(history_dataset, user_dataset)
+    print(type(transformed_data))
     prediction = train_model(transformed_data)
     if prediction == 0:
-        st.write("Parabéns, você é um cliente Bronze")
-    elif prediction == 1:
         st.write("Parabéns, você é um cliente Prata")
-    else:
+    elif prediction == 1:
         st.write("Parabéns, você é um cliente Ouro")
+    else:
+        st.write("Parabéns, você é um cliente Bronze")
 else:
     st.write('Aguardando Dados')
 
